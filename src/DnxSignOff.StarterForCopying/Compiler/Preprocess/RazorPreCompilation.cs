9bd6ab066875afce83ad68dbd129963463dc83eb
anurse <andrew@andrewnurse.net>
2015-03-31 09:44:33 -0700

    initial commit

---
+++ src/DnxSignOff.StarterForCopying/Compiler/Preprocess/RazorPreCompilation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace DnxSignOff.StarterForCopying.Compiler.Preprocess
+{
+    public class RazorPreCompilation : RazorPreCompileModule
+    {
+        public RazorPreCompilation(IServiceProvider provider) : base(provider)
+        {
+        }
+    }
+}
